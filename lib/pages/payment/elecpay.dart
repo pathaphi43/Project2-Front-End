aff27f15dd12d56d742d8640325278ad3600af78
sosadtvt <61011212049@msu.ac.th>
2021-08-10 19:51:37 +0700

    Doi

---
+++ lib/pages/payment/elecpay.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ElecpayPage extends StatefulWidget {
+  @override
+  _ElecpayPageState createState() => _ElecpayPageState();
+}
+
+class _ElecpayPageState extends State<ElecpayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/payment/waterpay.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WaterpayPage extends StatefulWidget {
+  @override
+  _WaterpayPageState createState() => _WaterpayPageState();
+}
+
+class _WaterpayPageState extends State<WaterpayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
